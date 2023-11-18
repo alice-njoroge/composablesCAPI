2c32444c71d7de44cd3b1e502675db6220fd6ce9
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-11-18 11:50:30 +0300

    introduction to composables

---
+++ src/composables/usePost.js
@@ -0,0 +1,16 @@
+import {ref} from "vue";
+
+export default function usePost() {
+
+    const posts = ref([]);
+    const fetchAll = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        posts.value = await response.json()
+    }
+
+    return{
+        fetchAll,
+        posts
+    }
+
+}
